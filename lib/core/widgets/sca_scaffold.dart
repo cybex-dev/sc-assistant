cca1b1c67a0348a7998ba72ad9c4d2a93b71e225
Charles Dyason <k4gcybex@gmail.com>
2023-06-28 10:16:43 +0200

    feat(drawer): add suggestions & issues links

--- lib/core/widgets/sca_scaffold.dart
+++ lib/core/widgets/sca_scaffold.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:sc_client/core/widgets/dialogs.dart';
 import 'package:sc_client/core/widgets/sca_drawer.dart';
 import 'package:sc_client/features/funds_disperser/presentation/pages/funds_disperser_page.dart';
 import 'package:sc_client/features/prison_timer/presentation/pages/prison_timer_page.dart';
+import 'package:url_launcher/url_launcher.dart';
 
+import '../const/urls.dart';
 import 'sca_appbar.dart';
 
 class SCAScaffold extends StatelessWidget {
@@ -57,8 +60,72 @@ class SCAScaffold extends StatelessWidget {
           const Spacer(),
           const _VersionBuilder(),
         ],
+        footer: const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: _FooterBuilder(),
+        ),
       ),
       body: body,
+    );
+  }
+}
+
+class _FooterBuilder extends StatelessWidget {
+  const _FooterBuilder({Key? key}): super(key: key);
+
+  void openLinkOrFail(BuildContext context, String link) {
+    final uri = Uri.parse(link);
+    canLaunchUrl(uri).then((value) => launchUrl(uri)).catchError((err) {
+      showPopup(
+        context: context,
+        title: "Error",
+        content: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text("Unfortunately, I could not open the link for you. Please visit:"),
+            InkWell(
+              onTap: () {
+                launchUrl(Uri.parse(github));
+              },
+              child: Text(
+                github,
+                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+              ),
+            ),
+          ],
+        ),
+      );
+      return false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextButton(
+          onPressed: () => openLinkOrFail(context, github),
+          child: const Text("Suggestion? Bug? Click here!"),
+        ),
+        const SizedBox(height: 8),
+        Row(
+          children: [
+            const Expanded(
+              child: _VersionBuilder(),
+            ),
+            IconButton(
+              onPressed: () => openLinkOrFail(context, githubIssues),
+              icon: Image.asset(
+                "assets/images/github.png",
+                height: 24.0,
+                fit: BoxFit.contain,
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
